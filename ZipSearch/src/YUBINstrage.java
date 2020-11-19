b65e1adbe4a86b66561c0b6d775d9a5322ec4147
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2020-11-19 19:56:34 +0900

    lengthにすることによって汎用性を高めた

--- ZipSearch/src/YUBINstrage.java
+++ ZipSearch/src/YUBINstrage.java
@@ -3,7 +3,7 @@ public class YUBINstrage {
 	String Yinfo[] = new String[5];
 
 	public YUBINstrage(String a[]) {
-		for(int i = 0;i < 5;i++) {
+		for(int i = 0;i < a.length;i++) {
 			Yinfo[i] = a[i];
 		}
 	}
@@ -13,7 +13,7 @@ public class YUBINstrage {
 		}
 
 	public void print() {
-		for(int i=0;i < 5;i++) {
+		for(int i=0;i < Yinfo.length;i++) {
 			System.out.print(Yinfo[i]+" ");
 		}
 		System.out.println();
